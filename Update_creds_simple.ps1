8c132c9c48f3ec378ae4cb38aed1224e3699dd27
crististanciu88 <63504338+crististanciu88@users.noreply.github.com>
2023-04-03 18:36:36 +0300

    Update Update_creds_simple.ps1

--- Update_creds_simple.ps1
+++ Update_creds_simple.ps1
@@ -1,25 +1,33 @@
-﻿# Define variables
-$username = "domain\username"
-$newpassword = "newpassword"
-$iisAppPools = Get-ChildItem IIS:\AppPools
-$iisSites = Get-ChildItem IIS:\Sites
-$iisVDirs = Get-ChildItem IIS:\Sites\*\*
+# Specify the new password and the desired user
+$newPassword = "NewPassword123"
+$desiredUser = "mydomain\myuser"
 
-# Update the credentials for the IIS app pools
-foreach ($appPool in $iisAppPools) {
-    Set-ItemProperty $appPool.PSPath -Name processModel.username -Value $username
-    Set-ItemProperty $appPool.PSPath -Name processModel.password -Value $newpassword
+# Get the current user's name
+$currentUserName = (Get-WmiObject Win32_ComputerSystem).UserName
+
+# Check if the current user is the desired user
+if ($currentUserName -eq $desiredUser) {
+    # Update the password for all application pools
+    Get-ChildItem "IIS:\AppPools" | ForEach-Object {
+        $poolName = $_.Name
+        Write-Host "Updating password for application pool: $poolName"
+        & "$env:SystemRoot\system32\inetsrv\appcmd.exe" set apppool "$poolName" -processModel.password:"$newPassword"
+    }
+
+    # Update the password for all IIS sites
+    Get-ChildItem "IIS:\Sites" | ForEach-Object {
+        $siteName = $_.Name
+        Write-Host "Updating password for site: $siteName"
+        & "$env:SystemRoot\system32\inetsrv\appcmd.exe" set site "$siteName" -applicationDefaults.password:"$newPassword"
+    }
+
+    # Update the password for all virtual directories
+    Get-ChildItem "IIS:\Sites" | Get-ChildItem -Recurse | Where-Object { $_.PSIsContainer -eq $false } | ForEach-Object {
+        $vdName = $_.Name
+        $siteName = $_.PSPath.Split('/', 4)[-1]
+        Write-Host "Updating password for virtual directory: $siteName/$vdName"
+        & "$env:SystemRoot\system32\inetsrv\appcmd.exe" set vdir "$siteName/$vdName" -password:"$newPassword"
+    }
+} else {
+    Write-Warning "This script can only be run by $desiredUser. Please run the script under $desiredUser's account."
 }
-
-# Update the credentials for the IIS sites
-foreach ($site in $iisSites) {
-    Set-ItemProperty $site.PSPath -Name applicationPool -Value $site.applicationPool.Replace($site.applicationPool.Split('\')[1], $username.Split('\')[1])
-}
-
-# Update the credentials for the IIS virtual directories
-foreach ($vdir in $iisVDirs) {
-    Set-ItemProperty $vdir.PSPath -Name applicationPool -Value $vdir.applicationPool.Replace($vdir.applicationPool.Split('\')[1], $username.Split('\')[1])
-}
-
-# Restart the IIS service
-Restart-Service W3SVC
