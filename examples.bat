d85fd3ac157928ef790e36c40733c4aec4a2fac9
crististanciu88 <63504338+crististanciu88@users.noreply.github.com>
2023-04-05 11:03:16 +0300

    Update examples.bat

--- examples.bat
+++ examples.bat
@@ -1,101 +1,23 @@
-﻿@echo off
-setlocal
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
 
-set "username=DOMAIN\USERNAME"
-set "newPassword=NEW_PASSWORD"
+[ApiController]
+public class ConfigsController : ControllerBase
+{
+    // GET: /Configs/config.xml
+    [HttpGet("Configs/config.xml")]
+    [Produces("application/xml")]
+    public IActionResult Get()
+    {
+        string filePath = Path.Combine(Directory.GetCurrentDirectory(), "config.xml");
 
-for /f "tokens=2" %%i in ('appcmd list apppool /state:Started /text:ProcessModel.UserName /value') do (
-    if /i "%%i"=="%username%" (
-        appcmd set apppool /apppool.name:"%%~i" /processmodel.username:"%username%" /processmodel.password:"%newPassword%"
-    )
-)
+        if (!System.IO.File.Exists(filePath))
+        {
+            return NotFound();
+        }
 
-for /f "tokens=2" %%i in ('appcmd list site /state:Started /text:applicationPool /value') do (
-    set "siteAppPool=%%i"
-    set "siteAppPoolUsername=!siteAppPool:%username:~8%=%username%!"
-    if /i "!siteAppPoolUsername!"=="%username%" (
-        appcmd set site /site.name:"%%~nxi" /applicationPool:"!siteAppPool:%username:~8%=%username%!"
-    )
-)
+        string fileContents = System.IO.File.ReadAllText(filePath);
 
-for /f "tokens=2" %%i in ('appcmd list vdir /state:Started /text:applicationPool /value') do (
-    set "vdirAppPool=%%i"
-    set "vdirAppPoolUsername=!vdirAppPool:%username:~8%=%username%!"
-    if /i "!vdirAppPoolUsername!"=="%username%" (
-        appcmd set vdir /vdir.name:"%%~i" /applicationPool:"!vdirAppPool:%username:~8%=%username%!"
-    )
-)
-
-net stop w3svc
-net start w3svc
-
-net user %username% %newPassword%
-____________________________________________________________
-
-@echo off
-setlocal
-
-set "username=DOMAIN\USERNAME"
-set "newPassword=NEW_PASSWORD"
-
-net user %username% %newPassword%
-
-for /f "tokens=2" %%i in ('appcmd list apppool /state:Started /text:ProcessModel.UserName /value') do (
-    if /i "%%i"=="%username%" (
-        appcmd set apppool /apppool.name:"%%~i" /processmodel.password:"%newPassword%"
-    )
-)
-
-for /f "tokens=2" %%i in ('appcmd list site /state:Started /text:applicationPool /value') do (
-    set "siteAppPool=%%i"
-    set "siteAppPoolUsername=!siteAppPool:%username:~8%=%username%!"
-    if /i "!siteAppPoolUsername!"=="%username%" (
-        appcmd set site /site.name:"%%~nxi" /applicationPool:"!siteAppPool:%username:~8%=%username%!"
-    )
-)
-
-for /f "tokens=2" %%i in ('appcmd list vdir /state:Started /text:applicationPool /value') do (
-    set "vdirAppPool=%%i"
-    set "vdirAppPoolUsername=!vdirAppPool:%username:~8%=%username%!"
-    if /i "!vdirAppPoolUsername!"=="%username%" (
-        appcmd set vdir /vdir.name:"%%~i" /applicationPool:"!vdirAppPool:%username:~8%=%username%!"
-    )
-)
-
-net stop w3svc
-net start w3svc
-
-_____________________________
-
-@echo off
-setlocal
-
-set "username=DOMAIN\USERNAME"
-set "newPassword=NEW_PASSWORD"
-
-dsmod user %username% -pwd %newPassword%
-
-for /f "tokens=2" %%i in ('appcmd list apppool /state:Started /text:ProcessModel.UserName /value') do (
-    if /i "%%i"=="%username%" (
-        appcmd set apppool /apppool.name:"%%~i" /processmodel.password:"%newPassword%"
-    )
-)
-
-for /f "tokens=2" %%i in ('appcmd list site /state:Started /text:applicationPool /value') do (
-    set "siteAppPool=%%i"
-    set "siteAppPoolUsername=!siteAppPool:%username:~8%=%username%!"
-    if /i "!siteAppPoolUsername!"=="%username%" (
-        appcmd set site /site.name:"%%~nxi" /applicationPool:"!siteAppPool:%username:~8%=%username%!"
-    )
-)
-
-for /f "tokens=2" %%i in ('appcmd list vdir /state:Started /text:applicationPool /value') do (
-    set "vdirAppPool=%%i"
-    set "vdirAppPoolUsername=!vdirAppPool:%username:~8%=%username%!"
-    if /i "!vdirAppPoolUsername!"=="%username%" (
-        appcmd set vdir /vdir.name:"%%~i" /applicationPool:"!vdirAppPool:%username:~8%=%username%!"
-    )
-)
-
-net stop w3svc
-net start w3svc
+        return Content(fileContents);
+    }
+}
