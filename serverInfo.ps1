7b1307ca187a379f72adee13c5e655ec59952162
crististanciu88 <63504338+crististanciu88@users.noreply.github.com>
2023-04-24 21:37:44 +0300

    Update serverInfo.ps1

--- serverInfo.ps1
+++ serverInfo.ps1
@@ -6,24 +6,20 @@ function Get-HardwareInfo {
     )
 
     $hardware = Invoke-Command -ComputerName $ServerName -ScriptBlock {
-        $computerSystem = Get-CimInstance -ClassName Win32_ComputerSystem
-        $bios = Get-CimInstance -ClassName Win32_BIOS
         $processor = Get-CimInstance -ClassName Win32_Processor
         $memory = Get-CimInstance -ClassName Win32_PhysicalMemory
-        $disk = Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DeviceID = 'C:'"
+        $diskC = Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DeviceID = 'C:'"
+        $diskD = Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DeviceID = 'D:'"
 
         [PSCustomObject]@{
-            'ServerName' = $computerSystem.Name
-            'Manufacturer' = $computerSystem.Manufacturer
-            'Model' = $computerSystem.Model
-            'BIOSVersion' = $bios.SMBIOSBIOSVersion
+            'ServerName' = $env:COMPUTERNAME
             'ProcessorName' = $processor.Name
             'NumberOfCores' = $processor.NumberOfCores
             'TotalPhysicalMemoryGB' = [Math]::Round($memory.Capacity | Measure-Object -Sum | Select-Object -ExpandProperty Sum / 1GB)
-            'DiskSizeGB' = [Math]::Round($disk.Size / 1GB)
+            'DiskCSizeGB' = [Math]::Round($diskC.Size / 1GB)
+            'DiskDSizeGB' = [Math]::Round($diskD.Size / 1GB)
         }
     }
 
     return $hardware
 }
-Get-HardwareInfo -ServerName "SERVER_NAME"
