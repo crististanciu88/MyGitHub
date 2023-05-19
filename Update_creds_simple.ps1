027d9a67e27065633b00fe251802f9e855708079
crististanciu88 <63504338+crististanciu88@users.noreply.github.com>
2023-05-19 09:38:11 +0300

    Update Update_creds_simple.ps1

--- Update_creds_simple.ps1
+++ Update_creds_simple.ps1
@@ -1,33 +1,15 @@
-# Specify the new password and the desired user
-$newPassword = "NewPassword123"
-$desiredUser = "mydomain\myuser"
+# Define the path to the .config file
+$configFilePath = "C:\path\to\your\config.file"
 
-# Get the current user's name
-$currentUserName = (Get-WmiObject Win32_ComputerSystem).UserName
+# Define the new password
+$newPassword = "new_password"
 
-# Check if the current user is the desired user
-if ($currentUserName -eq $desiredUser) {
-    # Update the password for all application pools
-    Get-ChildItem "IIS:\AppPools" | ForEach-Object {
-        $poolName = $_.Name
-        Write-Host "Updating password for application pool: $poolName"
-        & "$env:SystemRoot\system32\inetsrv\appcmd.exe" set apppool "$poolName" -processModel.password:"$newPassword"
-    }
+# Read the .config file as text
+$configContent = Get-Content -Path $configFilePath -Raw
 
-    # Update the password for all IIS sites
-    Get-ChildItem "IIS:\Sites" | ForEach-Object {
-        $siteName = $_.Name
-        Write-Host "Updating password for site: $siteName"
-        & "$env:SystemRoot\system32\inetsrv\appcmd.exe" set site "$siteName" -applicationDefaults.password:"$newPassword"
-    }
+# Find and replace the password value
+$newConfigContent = $configContent -replace "(?<=<Webpassword>).*?(?=</Webpassword>)", $newPassword
 
-    # Update the password for all virtual directories
-    Get-ChildItem "IIS:\Sites" | Get-ChildItem -Recurse | Where-Object { $_.PSIsContainer -eq $false } | ForEach-Object {
-        $vdName = $_.Name
-        $siteName = $_.PSPath.Split('/', 4)[-1]
-        Write-Host "Updating password for virtual directory: $siteName/$vdName"
-        & "$env:SystemRoot\system32\inetsrv\appcmd.exe" set vdir "$siteName/$vdName" -password:"$newPassword"
-    }
-} else {
-    Write-Warning "This script can only be run by $desiredUser. Please run the script under $desiredUser's account."
-}
+# Save the updated content back to the .config file
+$newConfigContent | Set-Content -Path $configFilePath
+
