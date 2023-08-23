26df7cdd8c9efe75d212e7250f07ca1c5480d976
crististanciu88 <63504338+crististanciu88@users.noreply.github.com>
2023-08-23 16:41:41 +0300

    Update serverInfo.ps1

--- serverInfo.ps1
+++ serverInfo.ps1
@@ -2,39 +2,33 @@ function Get-HardwareInfo {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory=$true)]
-        [string]$ServerName
+        [string[]]$ServerNames
     )
 
-    $hardware = Invoke-Command -ComputerName $ServerName -ScriptBlock {
-        $processor = Get-CimInstance -ClassName Win32_Processor
-        $memory = Get-CimInstance -ClassName Win32_PhysicalMemory
-        $diskC = Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DeviceID = 'C:'"
-        $diskD = Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DeviceID = 'D:'"
+    $results = @()
 
-        $services = Get-Service | Where-Object {$_.Name -like 'test*'} | ForEach-Object {
-            $serviceInfo = Get-WmiObject -Class Win32_Service -Filter "Name = '$($_.Name)'"
+    foreach ($ServerName in $ServerNames) {
+        $hardware = Invoke-Command -ComputerName $ServerName -ScriptBlock {
+            $processor = Get-CimInstance -ClassName Win32_Processor
+            $memory = Get-CimInstance -ClassName Win32_PhysicalMemory
+            $diskC = Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DeviceID = 'C:'"
+            $diskD = Get-CimInstance -ClassName Win32_LogicalDisk -Filter "DeviceID = 'D:'"
+                    
             [PSCustomObject]@{
-                'Name' = $serviceInfo.Name
-                'Status' = $serviceInfo.State
-                'BinaryPath' = $serviceInfo.PathName
-                'StartName' = $serviceInfo.StartName
-                'StartMode' = $serviceInfo.StartMode
+                'ServerName' = $env:COMPUTERNAME
+                'ProcessorName' = $processor.Name
+                'NumberOfCores' = ($processor | Measure-Object -Property NumberOfCores -Sum).Sum
+                'TotalPhysicalMemoryGB' = [Math]::Round($memory.Capacity | Measure-Object -Sum | Select-Object -ExpandProperty Sum / 1GB)
+                'DiskCSizeGB' = [Math]::Round($diskC.Size / 1GB)
+                'DiskDSizeGB' = [Math]::Round($diskD.Size / 1GB)
             }
         }
-                
-        [PSCustomObject]@{
-            'ServerName' = $env:COMPUTERNAME
-            'ProcessorName' = $processor.Name
-            'NumberOfCores' = $processor.NumberOfCores
-            'TotalPhysicalMemoryGB' = [Math]::Round($memory.Capacity | Measure-Object -Sum | Select-Object -ExpandProperty Sum / 1GB)
-            'DiskCSizeGB' = [Math]::Round($diskC.Size / 1GB)
-            'DiskDSizeGB' = [Math]::Round($diskD.Size / 1GB)
-            'ServicesInfo' = $services
-        }
+
+        $results += $hardware
     }
 
-    $hardware
+    $results | Format-Table -AutoSize
 }
 
 # Usage
-# $hardwareInfo = Get-HardwareInfo -ServerName "ServerName"
+# $hardwareInfo = Get-HardwareInfo -ServerNames @("Server1", "Server2", "Server3")
