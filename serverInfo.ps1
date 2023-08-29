e6091c5f923359a46b7f5a6a0e6281c9fc1cc80d
crististanciu88 <63504338+crististanciu88@users.noreply.github.com>
2023-08-29 15:00:11 +0300

    Update serverInfo.ps1

--- serverInfo.ps1
+++ serverInfo.ps1
@@ -1,64 +1,24 @@
-function Get-IISInfo {
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory=$true)]
-        [string]$ServerName,
-        [Parameter(Mandatory=$true)]
-        [string]$OutputPath
-    )
+# Import the IISAdministration module
+Import-Module IISAdministration
 
-    $iisSitesInfo = Invoke-Command -ComputerName $ServerName -ScriptBlock {
-        Import-Module WebAdministration
+# Get the computer name of the remote server
+$computerName = "localhost"
 
-        $iisSites = Get-Website
+# Connect to the remote server using Invoke-Command
+Invoke-Command -ComputerName $computerName {
 
-        $iisSites | ForEach-Object {
-            $bindingsInfo = $_.Bindings.Collection | ForEach-Object { "$($_.BindingInformation) ($($_.Protocol))" }
-            
-            [PSCustomObject]@{
-                'SiteName' = $_.Name
-                'BindingInformation' = $bindingsInfo -join '; '
-                'Protocol' = $_.Bindings.Collection.Protocol -join '; '
-                'PhysicalPath' = $_.PhysicalPath
-                'State' = $_.State
-            }
-        }
-    }
+  # Get the application pool names
+  Get-WebApplicationPool | Select-Object Name, State, Identity
 
-    $iisAppPoolsInfo = Invoke-Command -ComputerName $ServerName -ScriptBlock {
-        Import-Module WebAdministration
+  # Get the website names
+  Get-IISSite | Select-Object Name, State, Bindings
 
-        $iisAppPools = Get-WebAppPool
+  # Get the user under that application pool
+  Get-WebApplicationPool -Name $_.Name | Select-Object Identity
 
-        $iisAppPools | ForEach-Object {
-            [PSCustomObject]@{
-                'AppPoolName' = $_.Name
-                'AppPoolState' = $_.State
-            }
-        }
-    }
-
-    $combinedInfo = $iisAppPoolsInfo | ForEach-Object {
-        $appPool = $_
-        $site = $iisSitesInfo | Where-Object { $_.SiteName -eq $appPool.AppPoolName }
-
-        if ($site) {
-            $site | Select-Object *, @{Name='AppPoolState'; Expression={$appPool.AppPoolState}}
-        } else {
-            [PSCustomObject]@{
-                'SiteName' = $appPool.AppPoolName
-                'BindingInformation' = ''
-                'Protocol' = ''
-                'PhysicalPath' = ''
-                'State' = ''
-                'AppPoolName' = $appPool.AppPoolName
-                'AppPoolState' = $appPool.AppPoolState
-            }
-        }
-    }
-
-    $combinedInfo | Export-Csv -Path $OutputPath -NoTypeInformation
+  # Get the computer name
+  Get-ComputerName
 }
 
-# Usage
-# Get-IISInfo -ServerName "ServerName" -OutputPath "C:\Path\To\Output.csv"
+# Format the output as a table
+$results | Format-Table -AutoSize
